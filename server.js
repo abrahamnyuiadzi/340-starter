58b7c0637ceb514d12771c4b825f02f224955483
*abrahamnyuiadzi* <abrahamnyuiadzi>
2024-09-04 12:05:38 +0000

    login

--- server.js
+++ server.js
@@ -12,6 +12,8 @@ const app = express()
 const static = require("./routes/static")
 const baseController = require("./controllers/baseController")
 const inventoryRoute = require("./routes/inventoryRoute.js")
+const session = require("express-session")
+const pool = require('./database/')
 
 
 /* ***********************
@@ -20,6 +22,37 @@ const inventoryRoute = require("./routes/inventoryRoute.js")
 app.set("view engine", "ejs")
 app.use(expressLayouts)
 app.set("layout", "./layouts/layout") // not at views root
+
+
+
+
+/* ***********************
+ * Middleware
+ * ************************/
+app.use(session({
+  store: new (require('connect-pg-simple')(session))({
+    createTableIfMissing: true,
+    pool,
+  }),
+  secret: process.env.SESSION_SECRET,
+  resave: true,
+  saveUninitialized: true,
+  name: 'sessionId',
+}))
+
+
+// Express Messages Middleware
+app.use(require('connect-flash')())
+app.use(function(req, res, next){
+  res.locals.messages = require('express-messages')(req, res)
+  next()
+})
+
+
+
+
+
+
 
 /* ***********************
  * Routes
@@ -30,10 +63,37 @@ app.use( require("./routes/static"))
 app.get("/" ,baseController.buildHome)
 // Inventory routes
 app.use("/inv", inventoryRoute)
+//account  Route
+app.use("/account", require("./routes/accountRoute"))
 
+// File Not Found Route - must be last route in list
+app.use(async (req, res, next) => {
+  next({status: 404, message: 'Sorry, we appear to have lost that page.'})
+})
 
 
 app.use(static)
+
+
+
+
+
+
+
+
+/* ***********************
+* Express Error Handler
+* Place after all other middleware
+*************************/
+app.use(async (err, req, res, next) => {
+  let nav = await utilities.getNav()
+  console.error(`Error at: "${req.originalUrl}": ${err.message}`)
+  res.render("errors/error", {
+    title: err.status || 'Server Error',
+    message: err.message,
+    nav
+  })
+})
 
 /* ***********************
  * Local Server Information
