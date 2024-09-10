69b2b6cffb7055028871527801f8a15f0d6e2e79
*abrahamnyuiadzi* <abrahamnyuiadzi>
2024-09-10 17:24:43 +0000

    validator

--- routes/accountRoute.js
+++ routes/accountRoute.js
@@ -3,6 +3,7 @@ const express = require("express")
 const router = new express.Router() 
 const accountController = require("../controllers/accountController")
 const utilities = require("../utilities")
+const regValidate = require('../utilities/account-validation')
 
 /*** deliver a Login view */
 
@@ -11,10 +12,13 @@ router.get("/login",utilities.handleErrors(accountController.buildLogin))
 /***deliver registration view */
 router.get("/register",(accountController.buildRegister))
 
-/** route registration */
+// Process the registration data
 router.post(
-    "/register",utilities.handleErrors(accountController.registerAccount)
-)
+    "/register",
+    regValidate.registationRules(),
+    regValidate.checkRegData,
+    utilities.handleErrors(accountController.registerAccount)
+  )
 
 
 module.exports = router;
